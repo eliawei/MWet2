213853c970aece42fa89a13b3d0b24bf1fd370b8
DiscoRiko <gilchikorel@technion.ac.il>
2019-01-04 19:04:08 +0200

    StaticEye was added

---
+++ StaticEye.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gchiko on 04-Jan-19.
+//
+
+#include "StaticEye.h"
---
+++ StaticEye.h
@@ -0,0 +1,14 @@
+//
+// Created by Gchiko on 04-Jan-19.
+//
+
+#ifndef HASH_STATICEYE_H
+#define HASH_STATICEYE_H
+
+
+class StaticEye {
+
+};
+
+
+#endif //HASH_STATICEYE_H
